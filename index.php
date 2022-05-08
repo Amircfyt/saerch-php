c2a784b31bb61de07ba5c4b75b32ae635c8549fe
Amir cheraghi <100447221+Amircfyt@users.noreply.github.com>
2022-05-08 12:47:10 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for github :) </title>
+</head>
+<body>
+    
+<center>
+<form action="se.php" method="get">
+
+<div class="amir">
+<input class="input" type="search" name="q" id="">
+<button class="btn"  type="submit">
+<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="30px" height="30px"><g><g>
+	<g>
+		<path d="M225.474,0C101.151,0,0,101.151,0,225.474c0,124.33,101.151,225.474,225.474,225.474    c124.33,0,225.474-101.144,225.474-225.474C450.948,101.151,349.804,0,225.474,0z M225.474,409.323    c-101.373,0-183.848-82.475-183.848-183.848S124.101,41.626,225.474,41.626s183.848,82.475,183.848,183.848    S326.847,409.323,225.474,409.323z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#808080"/>
+	</g>
+</g><g>
+	<g>
+		<path d="M505.902,476.472L386.574,357.144c-8.131-8.131-21.299-8.131-29.43,0c-8.131,8.124-8.131,21.306,0,29.43l119.328,119.328    c4.065,4.065,9.387,6.098,14.715,6.098c5.321,0,10.649-2.033,14.715-6.098C514.033,497.778,514.033,484.596,505.902,476.472z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#808080"/>
+	</g>
+</g></g> </svg>
+</button>
+
+</div>
+
+</form>
+
+<style>
+.amir{
+width: 50%;
+background: #FFF;
+border-radius:8px;
+display: flex;
+height: 40px;
+
+}
+body{
+background:grey;
+}
+
+.btn{
+background:none;
+border:0px solid #FFF;
+}
+.input{
+    background:none;
+border:0px solid #FFF;
+outline: none;
+width:90%;
+}
+</style>
+
+
+
+
+</body>
+</html>
